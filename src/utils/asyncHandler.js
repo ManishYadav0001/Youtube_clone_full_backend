bf5c5a2a4040f60441f3b5f728d592897f3ed2e3
Manish yadav <krustermanish@gmail.com>
2025-08-15 15:07:54 +0530

    make 3 utility functions - For Errors, For response , For asyncHandling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+
+
+const asyncHandler = (func)=> async (req , res , next)=>{
+
+try {
+    
+    return await func(req, res , next)
+
+} catch (error) {
+   return res.status(error.status || 500).json(
+        {
+            success: false,
+            message:error.message
+        }
+
+    )
+}
+
+}
