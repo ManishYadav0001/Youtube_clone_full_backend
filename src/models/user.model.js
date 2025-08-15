1001ba8e84cf15e7626e97eec59823f183995dfd
Manish yadav <krustermanish@gmail.com>
2025-08-15 16:10:13 +0530

    added refreshToken and accessToken method in User.model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 const userSchema = mongoose.Schema({
     username: {
@@ -62,6 +63,41 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            fullName: this.fullName,
+            username: this.username
+        },
+
+        process.env.ACCESS_TOKEN_SECRET
+        ,
+
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+
+        },
+
+        process.env.REFRESH_TOKEN_SECRET
+        ,
+
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+
+        }
+    )
+}
 
 
 
