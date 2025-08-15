e611d3df2a06321eda829790a09b5383316d07ea
Manish yadav <krustermanish@gmail.com>
2025-08-15 14:29:38 +0530

    fix small errors

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     try {
 
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`MongoDB Connected Successfully ${connectionInstance}`)
+        console.log(`MongoDB Connected Successfully ${connectionInstance.connection.host}`)
 
     } catch (error) {
         console.log("MongoDB connection Error", error);
